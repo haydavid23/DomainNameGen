873742756137fd1080f84a7a7a181d43dd68f94b
David <hay.david@hotmail.com>
2019-03-17 01:09:25 +0000

    lastVersion

---
+++ index.js
@@ -0,0 +1,19 @@
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['jogger','racoon'];
+
+
+pronoun.forEach((x) => {
+  
+  adj.forEach((m) => {
+    
+  
+
+    noun.forEach((j) => {
+     console.log(x+m+j+".com");
+    
+    });
+  
+  });
+  
+});
